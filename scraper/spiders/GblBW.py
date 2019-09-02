78ea5f1ebfae98248d5ddf6bc7c28008a2f118e1
Fritz Mehler <f.mehler@gmail.com>
2019-09-03 00:59:09 +0200

    fix bw

--- scraper/spiders/GblBW.py
+++ scraper/spiders/GblBW.py
@@ -1,51 +1,25 @@
-import scrapy, re
+import scrapy, re 
 import datetime
 now = datetime.datetime.now()
+import urllib3
+http = urllib3.PoolManager()
 
 class NormSpider(scrapy.Spider):
     name = 'GesetzblattBW'
     year = str(now.year)
-    start_urls = ['http://www.landesrecht-bw.de/jportal/portal/t/dnn/page/bsbawueprod.psml/media-type/html?action=portlets.jw.ControlElementsAction&selectedcontrolelement=Verkuendungsblaetter']
+    start_urls = ['https://www.landtag-bw.de/files/live/sites/LTBW/files/dokumente/gesetzblaetter/'+year+'/GBl'+year+'01.pdf']
 
     def parse(self, response):
-
-   
-
-
-        # main page link to 
-        gblLink = response.xpath("//a[@class='HauptResultList'  and contains(.,'Gesetzblatt')]")
-        if gblLink:
-            href = response.urljoin(gblLink.css('::attr(href)').get())
-            print("link is",href)
-            request = scrapy.Request(url=href)
-            yield request
         
-        else:
-            print("response.text:",response.text)
+        for i in range(1,50):
+            nextUrl = re.sub("\d\d\.pdf","{:02d}".format(i)+".pdf",self.start_urls[0])
+            r = http.request('GET', nextUrl, preload_content=False)
+            if r.status==200:
 
+                yield {
+                    'ID': nextUrl.split("/")[-1].replace(".pdf",""),
+                    'Link': nextUrl
+                }
+            else:
+                break
 
-        #issue list
-        for item in response.xpath("//a[@class='TrefferlisteHervorheben' and contains(.,'Gesamtausgabe')]"):
-            href = response.urljoin(item.css('a::attr(href)').get())
-            request = scrapy.Request(url=href)
-            yield request
-
-
-
-        # #single issue
-        # if response.css("h1.mbottom_s_i"):
-        #     yield {
-        #         'ID': re.search("\d{4,}-.*$",response.request.url).group(0), # item.re_first('<h3>Fundstelle und systematische Gliederungsnummer</h3>.*?(?=<br>)')
-        #         'Titel': response.css("h1::text").get(), 
-        #         'Link': response.urljoin(response.css("#print_save_box a::attr(href)").get()), # a ::href
-        #         'Datum': re.search("Vollzitat:.{5,300}?(\d+\..{2,40}?\d{4})",response.body_as_unicode()).group(1).replace("&nbsp;"," "),
-        #         "Seiten": re.search(r"\(.{2,40}?(S\..*?)\)",response.body_as_unicode()).group(1).replace("&nbsp;"," ")
-        #     }
-
-
-        # # next search res page
-        # for item in response.css('.forward>a:first-child'):
-        #     href = response.urljoin(item.css('::attr(href)').get())
-        #     print("next res page",href)
-        #     request = scrapy.Request(url=href)
-        #     yield request
