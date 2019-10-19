ee3c8fc64083df228f26b2bc389c29db0c6c9a5f
Fritz Mehler <f.mehler@gmail.com>
2019-10-20 00:13:08 +0200

    misc

--- scraper/spiders/GblSaarland.py
+++ scraper/spiders/GblSaarland.py
@@ -6,20 +6,27 @@ http = urllib3.PoolManager()
 
 class NormSpider(scrapy.Spider):
     name = 'GesetzblattSaarland'
-    year = str(now.year)
-    start_urls = ['http://www.amtsblatt.saarland.de/jportal/docs/news_anlage/sl/pdf/VerkBl/ABl/ads_39-2019_teil_I_signed.pdf'+year+'/GBl'+year+'01.pdf']
+    start_urls = ['http://www.amtsblatt.saarland.de/jportal/portal/page/fpverksl.psml']
 
     def parse(self, response):
-        
-        for i in range(1,50):
-            nextUrl = re.sub("\d\d\.pdf","{:02d}".format(i)+".pdf",self.start_urls[0])
-            r = http.request('GET', nextUrl, preload_content=False)
-            if r.status==200:
+        for item in response.css('.newsInhalt'):
 
-                yield {
-                    'ID': nextUrl.split("/")[-1].replace(".pdf",""),
-                    'Link': nextUrl
-                }
-            else:
-                break
 
+            self.titel = item.css('a ::text').get().replace("Zum ","")
+            self.date = item.re_first('(?<=vom )\d+\. [A-ZÖÄÜ][a-zöäüß]+ \d{4}')
+            self.id = item.re_first('(?<=Nr\. )\d+')
+
+            nextUrl = response.urljoin(item.css('a::attr(href)').get())
+            request = scrapy.Request(url=nextUrl)
+            yield request
+
+        pdfIframe = response.css("#externIframe::attr(src)").get()
+        if pdfIframe:
+            yield {
+                'ID': self.id,
+                'Titel': self.titel,
+                'Link': response.urljoin(pdfIframe),
+                'Datum': self.date
+            }
+
+       
